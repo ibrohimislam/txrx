8fbdd024bf4399a2a35d6a2563b3c22cb125d876
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2015-09-20 06:56:10 +0700

    implement tx.cc

--- tx.cc
+++ tx.cc
@@ -8,54 +8,91 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
-mutex screenMutex;
-mutex writeFlag;
-
+// network io
 udp *udpSocket;
+int localPort;
+bool tx = true;
 
+// flow control
+mutex flagXOFF;
+condition_variable cv;
 bool receive_xoff = false;
 
-void transmitRoutine(string filename) {
-  ifstream ifile;
-  ifile.open(filename);
+// file
+ifstream ifile;
 
+void transmitRoutine() {
+  unique_lock<mutex> lck(flagXOFF);
+  
   int ibyte = 0;
+  char buffer;
 
+  ifile >> buffer;
   while (!ifile.eof()) {
-    //if (receive_xoff) wait();
-    char buffer;
-    ifile >> buffer;
+    
+    if (receive_xoff) {
+      cout << "waiting for XON" << endl;
+      cv.wait(lck);
+    }  
+    
     printf("Mengirim byte ke-%d: ‘%c’\n", ++ibyte, buffer);
     udpSocket->txchar(buffer);
+
+    ifile >> buffer;
   }
 
+  tx = false;
+  udpSocket->txchar(ETX);
   ifile.close();
 }
 
 void listenRoutine() {
-  
+  while (tx) {
+    char controlChar = udpSocket->rxchar();
+    
+    if (controlChar == XOFF) {
+      cout << "receive XOFF" << endl;
+      receive_xoff = true;
+    } else if (controlChar == XON) {
+      receive_xoff = false;
+      cv.notify_all();
+    }
+  }
 }
 
 int main(int argc, char* argv[])  {
   
-  if (argc < 2) {
-    cout << "usage: tx [filename]" << endl;
+  if (argc < 4) {
+    cout << "usage: tx [filename] [hostname] [port]" << endl;
     return 0;
   }
 
+  char* targetHost = argv[2];
+  int targetPort = atoi(argv[3]);
   string filename(argv[1]);
-
+  
+  ifile.open(filename);
+  ifile.unsetf(ios_base::skipws);
   udpSocket = new udp(0);
+  localPort = udpSocket->getLocalPort();
 
-  udpSocket->setTarget("127.0.0.1", 60000);
+  cout << "Listening on 0.0.0.0:" << localPort << endl;
 
-  std::thread transmitter(transmitRoutine, filename);
-  std::thread listenner(listenRoutine);
+  udpSocket->setTarget(targetHost, targetPort);
+
+  thread transmitter(transmitRoutine);
+  thread listenner(listenRoutine);
 
   transmitter.join();
+  
+  // self pipe
+  udpSocket->setTarget("127.0.0.1", localPort);
+  udpSocket->txchar(ETX);
+
   listenner.join();
   return 0;
 }
